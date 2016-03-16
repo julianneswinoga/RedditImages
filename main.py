d761babdc3349d981eab8a3d02ee5a938ec473f0
Julianne Swinoga <julianneswinoga@gmail.com>
2016-03-16 11:48:13 -0400

    Getting urls from files working

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import praw
 subreddits = []
 sub = []
 configFile = open("subreddits.config", "r")
+pictureLocation = configFile.readline()
+print pictureLocation
 for line in configFile:
     sub = line.replace("\n", "").split(",")
+    for i in range(len(sub)):
+        sub[i] = sub[i].strip()
     subreddits.append(sub)
 configFile.close()
 
-print subreddits
+R = praw.Reddit(user_agent="Background Image getter by /u/camca123")
 for sub in subreddits:
-    R = praw.Reddit(user_agent="Background Image getter by /u/camca123")
     if (sub[2] == "hour"):
-        submissions = R.get_subreddit(sub[0]).get_top_from_hour(limit = sub[1])
+        submissions = R.get_subreddit(sub[0]).get_top_from_hour(limit = int(sub[1]))
     elif (sub[2] == "day"):
-        submissions = R.get_subreddit(sub[0]).get_top_from_day(limit = sub[1])
+        submissions = R.get_subreddit(sub[0]).get_top_from_day(limit = int(sub[1]))
     elif (sub[2] == "week"):
-        submissions = R.get_subreddit(sub[0]).get_top_from_week(limit = sub[1])
+        submissions = R.get_subreddit(sub[0]).get_top_from_week(limit = int(sub[1]))
     elif (sub[2] == "month"):
-        submissions = R.get_subreddit(sub[0]).get_top_from_month(limit = sub[1])
+        submissions = R.get_subreddit(sub[0]).get_top_from_month(limit = int(sub[1]))
     elif (sub[2] == "year"):
-        submissions = R.get_subreddit(sub[0]).get_top_from_year(limit = sub[1])
+        submissions = R.get_subreddit(sub[0]).get_top_from_year(limit = int(sub[1]))
     elif (sub[2] == "all"):
-        submissions = R.get_subreddit(sub[0]).get_top_from_all(limit = sub[1])
+        submissions = R.get_subreddit(sub[0]).get_top_from_all(limit = int(sub[1]))
     for link in submissions:
         print link.url
